fdc69f15eb515261e32dcf015838c0ff50746b44
Suraj <53539666+moonstoper@users.noreply.github.com>
2020-09-08 23:46:19 +0530

    minors

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,15 +1,17 @@
 const burger = document.querySelector(".burger");
 const navlinks = document.querySelector(".navlinks");
-const links = document.querySelector(".navlinks li");
+const links = document.querySelectorAll(".navlinks li");
 const bar = document.querySelector(".bar");
 const navbar = document.querySelector("nav");
+const logo = document.querySelector(".logo");
 
 burger.addEventListener("click", () => {
   navlinks.classList.toggle("open");
   burger.classList.toggle("cross");
   navbar.classList.toggle("open");
   bar.classList.toggle("open");
-  links.forEach((link) => {
-    link.classList.toggle("fade");
-  });
+  // links.forEach((link) => {
+  //   link.classList.toggle("fade");
+  // });
+  logo.classList.toggle("open");
 });
