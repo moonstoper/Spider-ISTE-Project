823ed8b962c9d5412cfa063baaea7a63b96f0a9f
Suraj <53539666+moonstoper@users.noreply.github.com>
2020-09-08 18:29:15 +0530

    minor changes in header(animated)

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,12 +2,14 @@ const burger = document.querySelector(".burger");
 const navlinks = document.querySelector(".navlinks");
 const links = document.querySelector(".navlinks li");
 const bar = document.querySelector(".bar");
-const navbar =document.querySelector("nav");
+const navbar = document.querySelector("nav");
 
 burger.addEventListener("click", () => {
   navlinks.classList.toggle("open");
   burger.classList.toggle("cross");
-  navbar.classList.toggle("open")
+  navbar.classList.toggle("open");
   bar.classList.toggle("open");
-
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
 });
